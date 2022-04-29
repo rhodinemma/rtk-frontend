dd7ee678c9db901b1fbf91df0613b984df5fc2c4
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-04-30 00:07:55 +0300

    add files

---
+++ src/pages/Register.jsx
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCardFooter,
+  MDBValidation,
+  MDBBtn,
+  MDBIcon,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { register } from "../redux/features/authSlice";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const Register = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { email, password, firstName, lastName, confirmPassword } = formValue;
+  const { loading, error } = useSelector((state) => ({ ...state.auth }));
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      return toast.error("Passwords should match");
+    }
+    if (firstName && lastName && email && password && confirmPassword) {
+      dispatch(register({ formValue, navigate, toast }));
+    }
+  };
+
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "450px",
+        alignContent: "center",
+        marginTop: "120px",
+      }}
+    >
+      <MDBCard alignment="center">
+        <MDBIcon fas icon="user-circle" className="fa-2x" />
+        <h5>Register here</h5>
+        <MDBCardBody>
+          <MDBValidation onSubmit={handleSubmit} noValidate className="row g-3">
+            <div className="col-md-6">
+              <MDBInput
+                label="First Name"
+                type="text"
+                value={firstName}
+                name="firstName"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please provide first name"
+              />
+            </div>
+            <div className="col-md-6">
+              <MDBInput
+                label="Last Name"
+                type="text"
+                value={lastName}
+                name="lastName"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please provide last name"
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBInput
+                label="Email"
+                type="email"
+                value={email}
+                name="email"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please provide email"
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBInput
+                label="Password"
+                type="password"
+                value={password}
+                name="password"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please provide password"
+              />
+            </div>
+            <div className="col-md-12">
+              <MDBInput
+                label="Confirm password"
+                type="password"
+                value={confirmPassword}
+                name="confirmPassword"
+                onChange={onInputChange}
+                required
+                invalid
+                validation="Please confirm password"
+              />
+            </div>
+            <div className="col-12">
+              <MDBBtn style={{ width: "100%" }} className="mt-2">
+                {loading && (
+                  <MDBSpinner
+                    size="sm"
+                    role="status"
+                    tag="span"
+                    className="me-2"
+                  />
+                )}
+                Register
+              </MDBBtn>
+            </div>
+          </MDBValidation>
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link to="/login">
+            <p>Existing user? Login here</p>
+          </Link>
+        </MDBCardFooter>
+      </MDBCard>
+    </div>
+  );
+};
+
+export default Register;
